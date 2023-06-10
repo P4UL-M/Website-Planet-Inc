e8f963c51a8045d1d4a8be449764b8650e4516dc
AxelLns <axel.loones@gmail.com>
2023-06-10 18:57:13 +0200

    remove jquery use

--- js/planet.js
+++ js/planet.js
@@ -23,21 +23,36 @@
 
 // replace background image property of css with src of data-src attribute
 
-jQuery($ => {
-  let $background = $('.planet');
-  $background.each(function() {
-      let $this = $(this);
-      // get child element with class .ball
-      let $ball = $this.find('.ball');
-      let size = $this.data('size');
-      if (size) {
-        console.log(size);
-        $ball.css('transform', `scale(${size})`);
-      }
-      let src = $this.data('src');
-      if (src) {
-        console.log(src);
-        $ball.css('background-image', `url(${src})`);
-      }
-  });
+// jQuery($ => {
+//   let $background = $('.planet');
+//   $background.each(function() {
+//       let $this = $(this);
+//       // get child element with class .ball
+//       let $ball = $this.find('.ball');
+//       let size = $this.data('size');
+//       if (size) {
+//         console.log(size);
+//         $ball.css('transform', `scale(${size})`);
+//       }
+//       let src = $this.data('src');
+//       if (src) {
+//         console.log(src);
+//         $ball.css('background-image', `url(${src})`);
+//       }
+//   });
+// });
+
+// The previous function without jQuery
+
+const background = document.querySelectorAll('.planet'); 
+background.forEach(function(item) {
+    const ball = item.querySelector('.ball');
+    const size = item.dataset.size;
+    if (size) {
+        ball.style.transform = `scale(${size})`; 
+    }
+    const src = item.dataset.src;
+    if (src) {
+        ball.style.backgroundImage = `url(${src})`;
+    }
 });
